3e403379333211f2149d58758f02d070ce3e3faf
ShymaaIsmail <shymaa.m.ismail@gmail.com>
2023-10-15 19:25:46 +0300

    fix three  cases in task  (#11)

--- process_non_interactive_pipe.c
+++ process_non_interactive_pipe.c
@@ -8,11 +8,11 @@
 void process_non_interactive_pipe(char *program_name, int *exit_code)
 {
 	char *input;
-	char buffer[(BUFFER_SIZE * 2) + 1];
+	char buffer[(BUFFER_SIZE * 10) + 1];
 	ssize_t input_data_size;
 	char ***commands;
 
-	input_data_size = read(STDIN_FILENO, buffer, BUFFER_SIZE * 2);
+	input_data_size = read(STDIN_FILENO, buffer, BUFFER_SIZE * 10);
 	if (input_data_size > 0)
 	{
 		input = malloc(input_data_size + 1);
